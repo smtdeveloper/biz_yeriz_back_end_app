bd470bcbbe8fd499bd8a7082c22f83de372427bc
Samet Akca <smtakca99@gmail.com>
2024-09-23 23:37:37 +0300

    CompanyMappings  Name fix

---
+++ src/Core/bizyeriz.Application/Features/Companies/Profiles/CompanyMappings.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using bizyeriz.Application.Features.Companies.Queries.GetAllCompanies;
+using bizyeriz.Application.Features.Companies.Queries.GetCompanyById;
+using bizYeriz.Domain.Entities.CompanyEntities;
+
+namespace bizyeriz.Application.Features.Companies.Profiles;
+
+public class CompanyMappings : Profile
+{
+    public CompanyMappings()
+    {
+        CreateMap<Company, GetCompanyByIdQueryResponse>().ReverseMap();
+        CreateMap<Company, GetAllCompaniesQueryResponse>().ReverseMap();
+    }
+}
